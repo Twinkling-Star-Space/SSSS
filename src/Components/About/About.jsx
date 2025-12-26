f5f180918b19ba93a28a4ab1069a5358524034ba
Dip lal <xaverianbarahi111@gmail.com>
2025-12-26 18:18:41 +0545

    commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about_img.png'
+import play_icon from '../../assets/play_icon.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <img src={about_img} alt=""  className='about-img'/>
+            <img src={play_icon} alt=""  className='play_icon'/>
+        </div>
+        <div className="about-right">
+            <h3>ABOUT UNIVERSITY</h3>
+            <h2>Nurturing tomorrow leader today</h2>
+            <p>Our university is dedicated to providing exceptional education and fostering innovation. We strive to create an environment where students can grow intellectually and personally, preparing them to become leaders in their fields.</p>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/Campus/Campus.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Campus.css'
+import gallery1 from '../../Assets/gallery1.png'
+import gallery2 from '../../Assets/gallery2.png'
+import gallery3 from '../../Assets/gallery3.png'
+import gallery4 from '../../Assets/gallery4.png'
+import arrow from '../../Assets/dark-arrow.png'
+
+const Campus = () => {
+  return (
+    <div className='campus'>
+        <div className="gallery">
+            <img src={gallery1} alt="" />
+            <img src={gallery2} alt="" />
+            <img src={gallery3} alt="" />
+            <img src={gallery4} alt="" />
+        </div>
+        <button className="btn dark-btn">See More Here<img className='arrow' src={arrow} alt="" /></button>
+      
+    </div>
+  )
+}
+
+export default Campus
