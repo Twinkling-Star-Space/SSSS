c69058b10552e9ec272daea65dad8a41ee7c3e6c
Dip lal <xaverianbarahi111@gmail.com>
2025-12-27 20:53:14 +0545

    Fourth commit after completing tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
@@ -9,8 +9,12 @@ import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 import Quote from './Components/Quote/Quote'
+import Location from './Components/Location/Location'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar/>
@@ -21,19 +25,22 @@ const App = () => {
 
         <Title subTitle="Our PROGRAM" title="What We Offer"/>
         <Programs/>
-          <About/>
+          <About setPlayState={setPlayState}/>
           <Title subTitle="GALLERY" title="Campus Photos"/>
           <Campus/>
 
           <Title subTitle="TESTIMONIALS" title="What students say"/>
           <Testimonials/>
 
+          <Title subTitle="Location" title="We are here"/>
+          <Location/>
+
           <Title subTitle="Contact Us" title="Get in touch"/>
           <Contact/>
 
           <Footer/>
       </div>
-
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,12 +3,12 @@ import './About.css'
 import about_img from '../../assets/about_img.png'
 import play_icon from '../../assets/play_icon.png'
 
-const About = () => {
+const About = ({setPlayState}) => {
   return (
     <div className='about' name="about">
         <div className="about-left">
             <img src={about_img} alt=""  className='about-img'/>
-            <img src={play_icon} alt=""  className='play_icon'/>
+            <img src={play_icon} alt=""  className='play_icon' onClick={()=>{setPlayState(true)}}/>
         </div>
         <div className="about-right">
             <h3>ABOUT UNIVERSITY</h3>
